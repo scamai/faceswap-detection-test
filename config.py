e5af30eda5c661a2fd66a687ddc4ad55ce4a506d
jasperhu <jasper@metastate.bio>
2025-07-03 22:36:23 -0700

    Update gitignore
    Added config.py to keep it on track

---
+++ config.py
@@ -0,0 +1,5 @@
+# NOTE: For the API_URL, please contact our team to obtain the correct server address. Do not expose your server address in public repositories.
+API_URL = ''
+API_KEY = 'test123'
+DATASET_DIR = 'dataset'
+CSV_FILENAME = 'faceswap_detection_results.csv' 
